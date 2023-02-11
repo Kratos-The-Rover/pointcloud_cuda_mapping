e64b85a8228943e3056f0d2c9a9325a18e826d5d
AtharvMane <atharvmeenakshi@gmail.com>
2023-02-11 21:03:09 +0530

    Transformation Node A completed PointCloud GPU Transfer and Transform mechanism. Includes a skeleton for making elevation maps on CUDA. Most Basic Pointers are Used to keep compatibility with varying CUDA versions .

---
+++ include/pointcloud_cuda_mapping/pointcloud_nodelet.h
@@ -0,0 +1,14 @@
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#ifndef POINTCLOUD_NODELET_H
+#define POINTCLOUD_NODELET_H
+namespace pointcloud{
+    class Pointcloud{
+        public:
+            PointcloudProcessorNodelet();
+        private:
+            virtual void onInit();
+            cub
+    }
+}
+#endif
---
+++ src/pointcloud_nodelet.cpp
@@ -0,0 +1,13 @@
+
+#include <grid_map_msgs/GridMap.h>
+#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <grid_map_ros/GridMapRosConverter.hpp>
+namespace pointcloud
+{
+    
+    void Pointcloud::onInit(){
+
+    }
+} // namespace 
+PLUGINLIB_EXPORT_CLASS(planner::Plan, nodelet::Nodelet)
